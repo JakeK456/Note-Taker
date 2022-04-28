00cc6be50ce274760ea276d85b94103dc0353965
JK <jrileykruger@gmail.com>
2022-04-27 18:32:28 -0700

    added notes POST and DELETE routes

--- routes/notes.js
+++ routes/notes.js
@@ -7,6 +7,38 @@ notes.get('/', (req, res) => {
 });
 
 notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body){
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully.`);
+    } else {
+        res.error('Error in adding note.');
+    }
+});
+
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== id);
+
+        // Save that array to the filesystem
+         writeToFile('./db/db.json', result);
+
+        // Respond to the DELETE request
+        res.json(`Item ${id} has been deleted 🗑️`);
+    });
 
 });
 
